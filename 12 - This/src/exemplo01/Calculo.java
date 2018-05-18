2c4e884b29916be9f78a9d8e5146d084a96c9129
Joeezerah <39280295+Joeezerah@users.noreply.github.com>
2018-05-18 14:16:14 -0300

    12

---
+++ 12 - This/src/exemplo01/Calculo.java
@@ -0,0 +1,18 @@
+package exemplo01;
+
+public class Calculo {
+	
+	//Atributo da classe
+	public double resultado = 10;
+	
+	//Metodo para exibir um resultado
+	public void resultado () {
+		
+		//variavel local
+		double resultado = 5;
+		
+		//Exibir o resultado
+		System.out.println(this.resultado);
+	}
+
+}
